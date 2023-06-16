055817bfe49ab39ee892b7d6e727f1986cbc242e
dreambigatall <halidmahammed.16.hm@gmail.com>
2023-06-16 08:42:33 -0700

    molp

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 	int n;
-	int last;
+	int grass;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10;
+	grass = n % 10;
 
-	if (last > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, last);
-	else if (last == 0)
-		printf("Last digit of %i is %i and is 0\n", n, last);
-	else if (last < 6)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+	if (grass > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, grass);
+	else if (grass == 0)
+		printf("Last digit of %i is %i and is 0\n", n, grass);
+	else if (grass < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, grass);
 
 	return (0);
 }
