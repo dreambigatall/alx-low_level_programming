8639142db4e0c3119d1e7da8ab355476023a27f4
dreambigatall <halidmahammed.16.hm@gmail.com>
2023-06-16 08:01:15 -0700

    mkl

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char v = 'z';
+
+	while (v >= 'a')
+	{
+		putchar(c);
+		v--;
+	}
+
+	putchar('\n');
+	return (0);
+}
