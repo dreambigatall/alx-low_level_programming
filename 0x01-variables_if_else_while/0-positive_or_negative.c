fb0bd35f71306d1c9dbcbfa96e55c5e99281c68e
dreambigatall <halidmahammed.16.hm@gmail.com>
2023-06-16 05:55:46 -0700

    mu mi mi

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-printf("%d is posetive\n",n);
+printf("%d is posetive\n", n);
 }
 if (n < 0)
 {
-printf("%d is negative\n",n);
+printf("%d is negative\n", n);
 }
-if(n == 0 )
+if(n == 0)
 {
 printf("%d is zero\n", n);
 }
