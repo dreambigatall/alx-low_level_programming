b428e774fabb2c0014f7ddd281d71228f0f7a6a0
dreambigatall <halidmahammed.16.hm@gmail.com>
2023-07-03 05:23:17 -0700

    bona bona

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,17 @@
-include "main.h"
-
-/**
- * _memset - ironnou
- * @s: pointer to char params
- * @b: data to change
- * @n: index
- * Return: *s
- */
-
+#include "main.h"
+/** 
+* _memset - fill a block of memory with a specific value 
+* @s: starting address of memory to be filled 
+* @b: the desired value 
+* @n: number of bytes to be changed 
+* 
+* Return: changed array with new value for n bytes 
+*/
 char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
+int i = 0; 
+for (; n > 0; i++) 
+{ 
+s[i] = b; 
+n--; 
 }
+return (s);
