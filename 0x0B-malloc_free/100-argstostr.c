efca820e82fc18f4183f71c542222660bbc354b7
dreambigatall <halidmahammed.16.hm@gmail.com>
2023-07-10 05:04:37 -0700

    my dynamic memory allocation of heap memory by using pointer and array

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenate all arguments into a new string
+ * @ac: the argument count
+ * @av: the argument vector
+ *
+ * Return: NULL memory allocation fails, ac is 0 or av is NULL,
+ * otherwise return a pointer to the new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *cat, *chr;
+	int arg, size;
+
+	if (!ac || !av)
+		return (NULL);
+
+	for (arg = 0, size = 1; arg < ac; ++arg, ++size)
+	{
+		for (chr = av[arg]; *chr; ++chr, ++size)
+			;
+	}
+
+	cat = (char *) malloc(sizeof(char) * size);
+
+	if (!cat)
+		return (NULL);
+
+	for (arg = 0, size = 0; arg < ac; ++arg, ++size)
+	{
+		for (chr = av[arg]; *chr; ++chr, ++size)
+			cat[size] = *chr;
+		cat[size] = '\n';
+	}
+
+	cat[size] = '\0';
+
+	return (cat);
+}
